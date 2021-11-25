6be85c015c5ee5567a98f35908f004d7258d0b0a
masetty sai sundar <66826676+SaiSundarMasetty@users.noreply.github.com>
2021-11-25 10:11:23 +0530

    Add files via upload

---
+++ signup.php
@@ -0,0 +1,203 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <title>MOBIKART</title>
+  </head>
+  <style>
+
+.border-md {
+    border-width: 2px;
+}
+
+.btn-facebook {
+    background: #405D9D;
+    border: none;
+}
+
+.btn-facebook:hover, .btn-facebook:focus {
+    background: #314879;
+}
+
+.btn-twitter {
+    background: #42AEEC;
+    border: none;
+}
+
+.btn-twitter:hover, .btn-twitter:focus {
+    background: #1799e4;
+}
+
+body {
+    min-height: 100vh;
+}
+
+.form-control:not(select) {
+    padding: 1.5rem 0.5rem;
+}
+
+select.form-control {
+    height: 52px;
+    padding-left: 0.5rem;
+}
+
+.form-control::placeholder {
+    color: #ccc;
+    font-weight: bold;
+    font-size: 0.9rem;
+}
+.form-control:focus {
+    box-shadow: none;
+}
+
+      </style>
+  <body>
+      <?php
+      include 'header.php';
+      ?>
+ <div class="container">
+    <div class="row py-5 mt-4 align-items-center">
+     
+        <div class="col-md-5 pr-lg-5 mb-5 mb-md-0">
+            <img src="https://bootstrapious.com/i/snippets/sn-registeration/illustration.svg" alt="" class="img-fluid mb-3 d-none d-md-block">
+            <h2>Create an Account</h2   >
+            <p class="font-italic text-muted mb-0"></p>
+            </p>
+        </div>
+
+        <!-- Registeration Form -->
+        <div class="col-md-7 col-lg-6 ml-auto">
+            <form  action="signup_script.php" method="POST">
+                <div class="row">
+
+                    <!-- First Name -->
+                    <div class="input-group col-lg-6 mb-4">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text bg-white px-4 border-md border-right-0">
+                                <i class="fa fa-user text-muted"></i>
+                            </span>
+                        </div>
+                        <input id="firstName" type="text" name="f_name" placeholder="First Name" class="form-control bg-white border-left-0 border-md" pattern="[A-Za-z-0-9]+\s[A-Za-z-'0-9]+" required>
+                    </div>
+
+                    <!-- Last Name -->
+                    <div class="input-group col-lg-6 mb-4">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text bg-white px-4 border-md border-right-0">
+                                <i class="fa fa-user text-muted"></i>
+                            </span>
+                        </div>
+                        <input id="l_name" type="text" name="lastname" placeholder="Last Name" class="form-control bg-white border-left-0 border-md"pattern="[A-Za-z-0-9]+\s[A-Za-z-'0-9]+" required>
+                    </div>
+
+                    <!-- Email Address -->
+                    <div class="input-group col-lg-12 mb-4">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text bg-white px-4 border-md border-right-0">
+                                <i class="fa fa-envelope text-muted"></i>
+                            </span>
+                        </div>
+                        <input id="email" type="email" name="email" placeholder="Email Address" class="form-control bg-white border-left-0 border-md"pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$" Required>
+                        <?php
+                                if(isset($_GET["m1"])){
+                                  echo $_GET['m1'];
+                                }
+                                ?>
+                    </div>
+
+                    <!-- Phone Number -->
+                    <div class="input-group col-lg-12 mb-4">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text bg-white px-4 border-md border-right-0">
+                                <i class="fa fa-phone-square text-muted"></i>
+                            </span>
+                        </div>
+                        <select id="countryCode" name="countryCode" style="max-width: 80px" class="custom-select form-control bg-white border-left-0 border-md h-100 font-weight-bold text-muted" required>
+                            <option value="">+1</option>
+                            <option value="">+91</option>
+                            <option value="">+15</option>
+                            <option value="">+18</option>
+                        </select>
+                        <input id="phoneNumber" type="tel" name="p_no" placeholder="Phone Number" class="form-control bg-white border-md border-left-0 pl-3"pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}">
+                        <?php
+                                if(isset($_GET["m2"])){
+                                  echo $_GET['m2'];
+                                }
+                                ?>
+                    </div>.
+
+
+                    <!-- Job -->
+                    
+
+                    <!-- Password -->
+                    <div class="input-group col-lg-6 mb-4">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text bg-white px-4 border-md border-right-0">
+                                <i class="fa fa-lock text-muted"></i>
+                            </span>
+                        </div>
+                        <input id="pswd1" type="password" name="password" placeholder="Password" class="form-control bg-white border-left-0 border-md">
+                    </div>
+
+                    <!-- Password Confirmation -->
+                    <div class="input-group col-lg-6 mb-4">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text bg-white px-4 border-md border-right-0">
+                                <i class="fa fa-lock text-muted"></i>
+                            </span>
+                        </div>
+                        <input id="pswd2" type="text" name="passwordConfirmation" placeholder="Confirm Password" class="form-control bg-white border-left-0 border-md">
+                    </div>
+
+                    <!-- Submit Button -->
+                    <div class="form-group col-lg-12 mx-auto mb-0">
+                        <a href="#" class="btn btn-primary btn-block py-2" onclick="matchPassword()">
+                            <span class="font-weight-bold">Create your account</span>
+                        </a>
+                    </div>
+
+                     
+                    <!-- Already Registered -->
+                    <div class="text-center w-100">
+                        <p class="text-muted font-weight-bold">Already Registered? <a href="login.php" class="text-primary ml-2">Login</a></p>
+                    </div>
+
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+
+<script>
+function matchPassword() {
+  var pw1 = document.getElementById("pswd1");
+  var pw2 = document.getElementById("pswd2");
+  if(pw1 != pw2)
+  {	
+  	alert("Passwords did not match");
+  } else {
+  	alert("Account Created Successfully");
+  }
+}
+$(function () {
+    $('input, select').on('focus', function () {
+        $(this).parent().find('.input-group-text').css('border-color', '#80bdff');
+    });
+    $('input, select').on('blur', function () {
+        $(this).parent().find('.input-group-text').css('border-color', '#ced4da');
+    });
+});
+
+    </script>
+</body>
+<?php
+      include 'footer.php';
+      ?>
+</html>
